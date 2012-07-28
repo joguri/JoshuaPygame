05775850ebdf9aff5f68152a7657aa61251db00c
Jokichi Oguri <joe.oguri@hp.com>
2012-07-28 09:59:39 -0700

    Updated lesson4_done.py to be the starting point for lesson 5

--- lesson4_done.py
+++ lesson4_done.py
@@ -1,12 +1,13 @@
-import pygame,sys
+import pygame , sys
 
 screen = pygame.display.set_mode( (800,600) )
 
 background = pygame.image.load("Sky.png")
 backgroundRect = background.get_rect()
-pygame.key.set_repeat(1, 1)
 
 screen.blit(background,backgroundRect)
+
+pygame.key.set_repeat(200, 1)
 
 
 person = pygame.image.load("MR_bob.png")
@@ -18,6 +19,13 @@ screen.blit(person,personRect)
 
 pygame.display.flip()
 
+def Moveit():
+	i = 0
+	while (i < 20):
+		personRect.move_ip( [1,1] )
+		processTick()
+		i = i + 1
+
 def processTick():
 
 	screen.blit(background,backgroundRect)
@@ -26,9 +34,9 @@ def processTick():
 
 while 1==1:
 	for event in pygame.event.get():
-		if event.type==pygame.QUIT:
-			sys.exit()
 		if event.type==pygame.KEYDOWN and event.key == pygame.K_q:
+			exit()
+		if event.type==pygame.QUIT:
 			exit()
 		if event.type==pygame.KEYDOWN and event.key == pygame.K_UP:
 			personRect.move_ip( [0,-20] )
@@ -43,8 +51,7 @@ while 1==1:
 			personRect.move_ip( [-20,0] )
 			processTick()
 		if event.type==pygame.KEYDOWN and event.key == pygame.K_SPACE:
-			personRect.move_ip( [4,4] )
-			processTick()
+			Moveit()
 
 	
 
